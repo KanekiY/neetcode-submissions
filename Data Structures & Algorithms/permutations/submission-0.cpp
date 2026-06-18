55c6a52ceddfe9cd108cd96256e1e71f93c33336
KanekiY <62212307+KanekiY@users.noreply.github.com>
2026-06-18 10:39:50 +0100

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        vector<int> path;
+        vector<bool> used(nums.size(), false); // memorization
+
+        dfs(nums, used, path, result);
+
+        return result;
+    }
+
+    void dfs(
+        vector<int>& nums,
+        vector<bool>& used,
+        vector<int>& path,
+        vector<vector<int>>& result)
+    {
+        if(path.size() == nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(used[i])
+                continue;
+
+            used[i] = true;
+            path.push_back(nums[i]);
+
+            dfs(nums, used, path, result);
+
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};
