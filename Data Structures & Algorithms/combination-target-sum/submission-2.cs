9b94ad732a820a8de104fe1a05155958cf5a820b
KanekiY <62212307+KanekiY@users.noreply.github.com>
2026-07-07 18:06:29 +0100

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cs
@@ -0,0 +1,32 @@
+public class Solution {
+
+    public List<List<int>> allResults = new();
+    public List<List<int>> CombinationSum(int[] nums, int target) {
+       Array.Sort(nums, (a,b)=> a.CompareTo(b));
+       List<int> result = new();
+       dfs(nums,target,result,0);
+       return this.allResults;
+     
+    }
+    void dfs(int[] nums, int target, List<int> result, int index )
+    {
+        if(target == 0)
+        {
+            this.allResults.Add(new List<int>(result));
+        }
+        if(target < 0 || index > nums.Length-1)
+        {
+            return;
+        }
+
+        for(int i = index; i< nums.Length;i++)
+        {
+            if(nums[i]<=target)
+            {
+                result.Add(nums[i]);
+                dfs(nums, target - nums[i], result,i);
+                result.RemoveAt(result.Count-1);
+            }
+        }
+    }
+}
